03c79a06def67fb6ada8cb8d2226d8cd959bc62c
Keiichiro Ui <keiichiro.ui@gmail.com>
2015-01-07 02:22:36 +0900

    Fix link command

--- init.sh
+++ init.sh
@@ -1,33 +1,27 @@
-#!/bin/bash -exu
+#!/bin/bash
 # -*- coding:utf-8 -*-
+set -eu
 
 MACPORTS_INSTALLS=(git screen zsh curl wget coreutils)
 UBUNTU_INSTALLS=(git screen zsh curl wget ssh build-essential)
-PLATFORM="$(
-    if which lsb_release &>/dev/null && lsb_release -a | grep 'Ubuntu' &>/dev/null; then
-        echo "Ubuntu"
-    elif grep "darwin" <<< "$OSTYPE" &>/dev/null; then
-        echo "MacOSX"
-    else
-        echo ""
+BASE_DIR="$HOME/.dotfiles"
+LN="$(
+    if which gln &>/dev/null
+    then echo gln
+    else echo ln
     fi
 )"
-BASE_DIR="$HOME/.dotfiles"
 
 main() {
-    if [[ -z "$PLATFORM" ]]; then
-        abort "Not supported platform"
-    fi
-
     install_basics
     if [[ -e "$BASE_DIR" ]];
-    then cd "$BASE_DIR"; git push origin master
-    else git clone git@github.com:kui/kui_local.git "$BASE_DIR"
+    then cd "$BASE_DIR"; run git push origin master
+    else run git clone git@github.com:kui/kui_local.git "$BASE_DIR"
     fi
     cd "$BASE_DIR"
 
-    git submodule init
-    git submodule update
+    run git submodule init
+    run git submodule update
 
     install_dotfiles
     install_templates
@@ -38,19 +32,21 @@ main() {
 }
 
 install_basics() {
-    local installs
-    case "$PLATFORM" in
-        Ubuntu) for p in "${UBUNTU_INSTALLS[@]}"; do sudo apt-get install "$p"; done;;
-        MacOSX) for p in "${MACPORTS_INSTALLS[@]}"; do sudo port install "$p"; done;;
-        *) abort "Invalid platform"
-    esac
+    if which lsb_release &>/dev/null && lsb_release -a | grep 'Ubuntu' &>/dev/null; then
+        run sudo apt-get install "${UBUNTU_INSTALLS[@]}"
+    elif grep "darwin" <<< "$OSTYPE" &>/dev/null; then
+        run sudo port install "${MACPORTS_INSTALLS[@]}" ||
+            abort "Require MacPorts"
+    else
+        abort "Non supported platform"
+    fi
 }
 
 install_dotfiles() {
     local file
     for file in $(pwd)/dotfiles/*; do
         local dest="$HOME/.$(basename $file)"
-        ln_s "$file" "$dest"
+        run $LN -sbT "$file" "$dest"
     done
 }
 
@@ -62,28 +58,30 @@ install_templates() {
             warn "Skip the template installation: Already exist $file"
             continue
         fi
-        cp "$file" "$dest"
+        run cp "$file" "$dest"
     done
 }
 
-ln_s() {
-    local opts=$(
-        if   [[ "$PLATFORM" == "Ubuntu" ]]; then echo "-sbT"
-        elif [[ "$PLATFORM" == "MacOSX" ]]; then echo "-sf"
-        fi
-    )
-    ln "$opts" $1 $2
+run() {
+    echo_green $ "$@"
+    env "$@"
 }
 
+echo_green() {
+    echo "\e[32m${@}\e[39m"
+}
+echo_red() {
+    echo "\e[31m${@}\e[39m"
+}
 abort() {
-    err "$@"
+    err "Abort: $@"
     exit 1
 }
 err() {
-    echo -e "$@" >&2
+    echo_red "$@" >&2
 }
 warn() {
-    echo -e "WARN: $@"
+    echo_red "WARN: $@"
 }
 
 main
